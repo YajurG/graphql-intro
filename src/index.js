0e2ffeddcddc218d84c42d06df65e9b28686c42f
YajurG <yajurgrover24@gmail.com>
2021-12-05 17:18:54 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,13 +2,56 @@ const {ApolloServer, gql} = require('apollo-server');
 
 const typeDefs = gql `
  type Query {
-     hello: String
+     hello: String!
+     user: User
+ }
+
+ type Mutation {
+     register(userInfo: UserInfo!): RegisterResponse
+ }
+
+ type RegisterResponse {
+     user: User
+     errors: [Error]
+ }
+
+ type Error {
+     field: String!
+     message: String!
+ }
+
+ input UserInfo {
+     username: String,
+     password: String
+ }
+
+ type User {
+     id: ID!
+     name: String!
  }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => "Hello World"
+        hello: () => "Shut up bitch!",
+        user: () => ({
+            id: 1,
+            name: "Ben"
+        })
+    },
+    Mutation: {
+        register: () => ({
+            user: {
+                id: 1,
+                name: "John"
+            },
+            errors: [
+                {
+                    field: "name",
+                    message: "randome message lol"
+                }
+            ]
+        })
     }
 }
 
